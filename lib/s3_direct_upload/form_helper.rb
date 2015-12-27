6537369b7610c3b8711e3eac0c2812c87eb0525f
Robert Vizza <robertvizza@me.com>
2015-12-27 09:22:43 -0800

    extend aws policy expiration to 10 days

--- lib/s3_direct_upload/form_helper.rb
+++ lib/s3_direct_upload/form_helper.rb
@@ -24,7 +24,7 @@ module S3DirectUpload
           bucket: options[:bucket] || S3DirectUpload.config.bucket,
           ssl: true,
           acl: "public-read",
-          expiration: 10.hours.from_now.utc.iso8601,
+          expiration: 240.hours.from_now.utc.iso8601,
           max_file_size: 500.megabytes,
           callback_method: "POST",
           callback_param: "file",
